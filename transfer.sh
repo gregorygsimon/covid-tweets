3e182b1083693b9c6c7a35507bc0f269696d163f
Gregory G. Simon <gregorygsimon@gmail.com>
2020-08-25 18:03:26 -0400

    streamlined transfers.sh

--- transfer.sh
+++ transfer.sh
@@ -2,14 +2,21 @@ d=$(date +"%Y-%m-%d")
 
 FILE=/media/hdd_1tb/data/twitter-$d.db
 
-if [ -f "$FILE" ]; then
-    echo "$FILE already transfered."
+if [ -f "$FILE" ]
+then
+    echo "$FILE already transfered today."
 else
     scp gregorygsimon@emet:/home/gregorygsimon/scripts/covid-twitter/twitter.db "$FILE"
     ssh gregorygsimon@emet "cd /home/gregorygsimon/scripts/covid-twitter/; mv twitter.db twitter-$d.db"
-    source activate covidtweets
-    python processing-sentiment.py
 fi
+
+source activate covidtweets
+python processing-sentiment.py
+jupyter nbconvert --to notebook --inplace --execute EDA.ipynb
+git add *.png
+git commit -m "auto update for figures, $d"
+git push origin master
+
 
 
 
